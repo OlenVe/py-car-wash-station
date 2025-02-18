e9e0ffce730170421799c5decd8f3df19b1b696c
Dolhyi Valerii <v.dolhyi@gmail.com>
2025-02-18 17:02:12 +0100

    'Solution_2'

--- app/main.py
+++ app/main.py
@@ -20,20 +20,19 @@ class CarWashStation:
         for car in cars:
             if self.clean_power > car.clean_mark:
                 income += self.calculate_washing_price(car)
-        self.wash_single_car(cars)
+                self.wash_single_car(car)
         return income
 
-    def calculate_washing_price(self, car: callable) -> any:
+    def calculate_washing_price(self, car: Car) -> any:
         price = round(car.comfort_class
                       * (self.clean_power - car.clean_mark)
                       * (self.average_rating / self.distance_from_city_center),
                       1)
         return price
 
-    def wash_single_car(self, cars: list) -> any:
-        for car in cars:
-            if self.clean_power > car.clean_mark:
-                car.clean_mark = self.clean_power
+    def wash_single_car(self, car: Car) -> any:
+        if self.clean_power > car.clean_mark:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> any:
         if rate:
